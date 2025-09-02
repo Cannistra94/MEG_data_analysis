a202711f525bed9cfc0b3710adc7e890b378464b
mcannistra <95096730+Cannistra94@users.noreply.github.com>
2025-09-02 12:00:35 -0400

    Update FreeSurfer_preprocessing.sh

--- FreeSurfer_MRI_reconstruction/FreeSurfer_preprocessing.sh
+++ FreeSurfer_MRI_reconstruction/FreeSurfer_preprocessing.sh
@@ -1,1 +1,15 @@
+# Set FreeSurfer environment
+source /usr/local/freesurfer/stable7.4.1/SetUpFreeSurfer.sh  
 
+subject_id=$1
+
+# Define the T1-weighted MRI input file path
+t1_fname="${subject_id}/anat.nii.gz"
+
+# Define the output log file
+log_file="${subject_id}/my-recon-all.txt"
+
+# Define the output directory for FreeSurfer results
+subjects_output_dir="processed_t1_freesurfer_output"
+
+recon-all -all -s "$subject_id" -sd "$subjects_output_dir" -i "$t1_fname" &> "$log_file"
